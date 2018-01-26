005d917748f2cebd023511d69092b980e128a8e4
henstecj <henstecj@rose-hulman.edu>
2018-01-26 14:27:08 -0500

    added frontend boilerplate code

--- schedule-app/src/app/app.module.ts
+++ schedule-app/src/app/app.module.ts
@@ -1,8 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+//import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatRippleModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+
+} from '@angular/material';
+
+
+
+export const MaterialModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatRippleModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+];
 
 
 @NgModule({
@@ -10,7 +43,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModules,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
