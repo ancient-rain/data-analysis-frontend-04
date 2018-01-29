7275f68b4739e09b014190b39ad61c4d4a796b16
askalerion <bednartd@rose-hulman.edu>
2018-01-29 04:34:29 -0500

    Created empty scheulde to update based on class info for the term

--- schedule-app/src/app/models/course.ts
+++ schedule-app/src/app/models/course.ts
@@ -2,11 +2,11 @@ export class Course {
     constructor(
         public id: string,
         public type: string,
-        public name: string,
         public term: string,
+        public name: string,
         public description: string,
         public creditHours: string,
         public meetTimes: string,
-        public instructor: string
+        public instructor: string,
     ) { }
 }
--- schedule-app/src/app/student/student-term-info/student-schedule/student-schedule.component.ts
+++ schedule-app/src/app/student/student-term-info/student-schedule/student-schedule.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Student } from '../../../models/student';
 
-
 @Component({
   selector: 'app-student-schedule',
   templateUrl: './student-schedule.component.html',
@@ -9,10 +8,39 @@ import { Student } from '../../../models/student';
 })
 export class StudentScheduleComponent implements OnInit {
   @Input() student: Student;
+  days: [string];
+  hours: [number];
+  schedule;
 
-  constructor() { }
+  constructor() {
+    this.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    this.hours = [805, 900, 955, 1050, 1145, 1240, 1335, 1430, 1525, 1620];
+    this.createEmptySchedule();
+  }
+
+  createEmptySchedule() {
+    const scheduleDays = [];
+    for (let i = 0; i < this.days.length; i++) {
+      const scheduleHours = [];
+      for (let j = 0; j < this.hours.length; j++) {
+        const hour = {
+          hour: '' + j,
+          startTime: this.hours[j]
+        };
+        scheduleHours.push(hour);
+      }
+      const day = {
+        day: this.days[i],
+        hours: scheduleHours
+      };
+      scheduleDays.push(day);
+    }
+
+    this.schedule = scheduleDays;
+  }
 
   ngOnInit() {
+    console.log(this.schedule);
   }
 
 }
