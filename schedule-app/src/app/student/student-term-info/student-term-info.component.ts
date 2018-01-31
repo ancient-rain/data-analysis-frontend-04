92225d79eff6d85d766c7c404be9380770ee1b82
askalerion <bednartd@rose-hulman.edu>
2018-01-31 05:51:26 -0500

    Cleaned up student-term-info

--- schedule-app/src/app/student/student-term-info/student-term-info.component.ts
+++ schedule-app/src/app/student/student-term-info/student-term-info.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { Student } from '../../models/student';
 import { StudentService } from '../../services/student.service';
 import { EmitterService } from '../../services/emitter.service';
+import { FilterDataService } from '../../services/filter-data.service';
 import { Course } from '../../models/course';
 
 @Component({
@@ -23,10 +24,12 @@ export class StudentTermInfoComponent implements OnInit {
   finalLength: number;
   terms;
 
-  constructor(private studentService: StudentService, private router: Router) {
-    this.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    this.hours = [805, 900, 955, 1050, 1145, 1240, 1335, 1430, 1525, 1620];
-    this.finalLength = 400;
+  constructor(private studentService: StudentService,
+    private filterService: FilterDataService,
+    private router: Router) {
+    this.days = this.filterService.getDays();
+    this.hours = this.filterService.getHours();
+    this.finalLength = this.filterService.getFinalLength();
     this.getHeaders();
   }
 
@@ -40,19 +43,13 @@ export class StudentTermInfoComponent implements OnInit {
   loadStudent() {
     this.studentService.getStudentTermInfo(this.username, this.term)
       .subscribe(student => {
+        const data = student[0];
+
         this.student = student;
-        this.terms = student[0].terms;
+        this.terms = data.terms;
 
-        for (let i = 0; i < this.student[0].courses.length; i++) {
-          const course = this.student[0].courses[i];
-          let courseTime = course.meetTimes;
-          courseTime = courseTime.replace(/  /g, ' ');
-          const times = courseTime.split(' ');
-          const classTime = this.filterClass(courseTime, times);
-          course.filteredTimes = classTime;
-        }
-
-        this.updateSchedule(this.student[0].courses);
+        this.filterClasses(data);
+        this.filterService.updateSchedule(this.schedule, data.courses);
       },
       err => {
         this.router.navigate(['not-found']);
@@ -60,106 +57,18 @@ export class StudentTermInfoComponent implements OnInit {
       );
   }
 
-  filterClass(courseTime: string, times: [string]) {
-    for (let j = 0; j < times.length; j = j + 2) {
-      const days = times[j];
-      const hours = times[j + 1].split('-');
-      const start = parseInt(hours[0], 10);
-      const end = parseInt(hours[1], 10);
-
-      if (days.length <= 1) {
-        const isFinal = this.isFinal(start, end);
-        if (!isFinal) {
-          return this.getFilteredClass(days, start, end);
-        }
-      } else {
-        return this.getFilteredClass(days, start, end);
-      }
+  filterClasses(data: any) {
+    for (let i = 0; i < data.courses.length; i++) {
+      const course = data.courses[i];
+      const classTime = this.filterService.getClassTime(course);
+      course.filteredTimes = classTime;
     }
-  }
-
-  getFilteredClass(days: string, start: number, end: number) {
-    let isStarted = false;
-    let hoursStr = '';
-    for (let j = 0; j < this.hours.length; j++) {
-      if (start <= this.hours[j] && !isStarted) {
-        isStarted = true;
-        hoursStr += j + 1;
-      } else if (end <= this.hours[j] && isStarted) {
-        if (hoursStr !== `${j}`) {
-          hoursStr += `-${j}`;
-        }
-        break;
-      }
-    }
-    return `${days}/${hoursStr}`;
   }
 
   switchTerm(term: string) {
     this.router.navigate(['student', this.username, term]);
     /* without refreshing it won't load new content, not sure why */
     document.location.reload(true);
-  }
-
-  updateSchedule(courses: [Course]) {
-    for (let i = 0; i < courses.length; i++) {
-      let meetTimes = courses[i].meetTimes;
-      meetTimes = meetTimes.replace(/  /g, ' ');
-      const times = meetTimes.split(' ');
-
-      for (let j = 0; j < times.length; j = j + 2) {
-        const days = times[j];
-        const hours = times[j + 1].split('-');
-        const start = parseInt(hours[0], 10);
-        const end = parseInt(hours[1], 10);
-
-        if (days.length <= 1) {
-          const isFinal = this.isFinal(start, end);
-          if (!isFinal) {
-            this.addClass(days, courses[i].name, start, end);
-          }
-        } else {
-          this.addClass(days, courses[i].name, start, end);
-        }
-      }
-    }
-  }
-
-  isFinal(start, end) {
-    return end - start >= this.finalLength;
-  }
-
-  addClass(days: string, name: string, start: number, end: number) {
-    for (let i = 0; i < days.length; i++) {
-      const day = this.getDay(days.charAt(i));
-      let isStarted = false;
-      for (let j = 0; j < this.hours.length; j++) {
-        if (start <= this.hours[j] && !isStarted) {
-          this.schedule.set(`${day}-${this.hours[j]}`, `${name}`);
-          isStarted = true;
-        } else if (end >= this.hours[j] && isStarted) {
-          this.schedule.set(`${day}-${this.hours[j]}`, `${name}`);
-        } else if (end <= this.hours[j] && isStarted) {
-          break;
-        }
-      }
-    }
-  }
-
-  getDay(day: string) {
-    if (day === 'M') {
-      return 'Monday';
-    } else if (day === 'T') {
-      return 'Tuesday';
-    } else if (day === 'W') {
-      return 'Wednesday';
-    } else if (day === 'R') {
-      return 'Thursday';
-    } else if (day === 'F') {
-      return 'Friday';
-    } else {
-      return 'Saturday';
-    }
   }
 
   ngOnInit() {
