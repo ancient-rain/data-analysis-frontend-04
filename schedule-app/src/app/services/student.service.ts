619dffeeec59c4797a5dfe9e5205c7ed87c8bdb0
askalerion <bednartd@rose-hulman.edu>
2018-02-15 13:00:51 -0500

    Added my courses page

--- schedule-app/src/app/services/student.service.ts
+++ schedule-app/src/app/services/student.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Student } from '../models/student';
+import { StudentInfo } from '../models/student-info';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
@@ -9,6 +10,13 @@ export class StudentService {
   private domainUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
+
+  getStudentInfo(username: String): Observable<StudentInfo> {
+    const url = `${this.domainUrl}/student/${username}`;
+    return this.http.get<StudentInfo>(url)
+      .catch((error: any) =>
+        Observable.throw(error.json().error || 'Server error'));
+  }
 
   getStudentTermInfo(username: String, term: String): Observable<Student> {
     const url = `${this.domainUrl}/student/${username}/${term}`;
